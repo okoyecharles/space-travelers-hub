07102aed09294ffda3f53c2d42405b3c74c27de1
AngomRanjan <angomranjan@gmail.com>
2022-08-23 12:33:57 +0530

    Created Profile

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import { HashRouter, Routes } from 'react-router-dom';
+import { HashRouter, Routes, Route } from 'react-router-dom';
 import Header from './components/Header';
+import MyProfile from './components/MyProfile';
 
 function App() {
   return (
     <HashRouter>
       <Header />
-      <Routes />
+      <Routes>
+        <Route path="myProfile" element={<MyProfile />} />
+      </Routes>
     </HashRouter>
   );
 }

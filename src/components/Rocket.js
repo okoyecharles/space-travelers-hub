3936bae0bac1d0b84d2f9d0d1d3bc3e66baf3080
OkoyeCharles <okoyecharles509@gmail.com>
2022-08-23 10:46:47 +0100

    Create basic structure for rockets

---
+++ src/components/Rocket.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function Rocket() {
+  return (
+    <div className="rocket__container">
+      <div className="col m-3 p-3 border">
+        column to display rockets
+      </div>
+    </div>
+  );
+}
+
+export default Rocket;

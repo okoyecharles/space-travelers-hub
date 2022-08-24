3c8ab8adc589fa82b03145c829907658302433fa
OkoyeCharles <okoyecharles509@gmail.com>
2022-08-24 11:28:39 +0100

    Render rockets from redux store

--- src/components/Rocket.js
+++ src/components/Rocket.js
@@ -1,9 +1,32 @@
-import React from 'react';
+import React, { useState } from 'react';
+import PropTypes from 'prop-types';
 
-function Rocket() {
+function Rocket({
+  id, image, name, description,
+}) {
+  const [reserved, setReserved] = useState(false);
+  const handleClick = () => {
+    setReserved((prevState) => !prevState);
+  };
   return (
-    <div>Rocket</div>
+    <div className="rocket">
+      <div className="rocket__container">
+        <img className="rocket__image" src={image} alt={id} />
+        <div className="rocket__info">
+          <h2 className="rocket__title">{name}</h2>
+          <p className="rocket__description">{description}</p>
+          <button onClick={handleClick} type="button">{reserved ? 'Cancel Reservation' : 'Reserve Rocket'}</button>
+        </div>
+      </div>
+    </div>
   );
 }
+
+Rocket.propTypes = {
+  id: PropTypes.number.isRequired,
+  image: PropTypes.string.isRequired,
+  name: PropTypes.string.isRequired,
+  description: PropTypes.string.isRequired,
+};
 
 export default Rocket;
--- src/components/Rockets.js
+++ src/components/Rockets.js
@@ -1,19 +1,30 @@
 import React, { useEffect } from 'react';
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import fetchRockets from '../redux/rocket/actions';
+import Rocket from './Rocket';
 
 function Rockets() {
-  // const rockets = useSelector((state) => state.rockets);
+  const rockets = useSelector((state) => state.rockets);
   const dispatch = useDispatch();
 
   useEffect(() => {
-    dispatch(fetchRockets());
+    if (!rockets.length) dispatch(fetchRockets());
   }, []);
+
+  console.log(rockets);
 
   return (
     <div className="rocket__container">
       <div className="col m-3 p-3 border">
-        column to display rockets
+        {rockets.map((rocket) => (
+          <Rocket
+            key={rocket.id}
+            name={rocket.name}
+            id={rocket.rocketId}
+            description={rocket.description}
+            image={rocket.image}
+          />
+        ))}
       </div>
     </div>
   );

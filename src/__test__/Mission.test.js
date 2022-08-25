11e03d07d89d506e85f642f143ba30b0b3e9738b
AngomRanjan <angomranjan@gmail.com>
2022-08-25 13:50:04 +0530

    Add Test For Mission

---
+++ src/__test__/Mission.test.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import { Provider } from 'react-redux';
+import renderer from 'react-test-renderer';
+import '@testing-library/jest-dom';
+import Mission from '../components/Mission';
+import store from '../redux/configureStore';
+
+describe('Test for Rockets', () => {
+  test('should render', () => {
+    const msn = {
+      id: 1,
+      name: 'dfg',
+      description: 'ghdj',
+      reserved: false,
+    };
+    const tree = renderer.create(
+      <Provider store={store}>
+        <Mission mission={msn} key={1} />
+      </Provider>,
+    );
+    expect(tree).toMatchSnapshot();
+  });
+});
